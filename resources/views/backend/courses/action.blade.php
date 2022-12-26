82717826f0835402359dc0142fa8fbb4d8326f19
fouad mekkey <codepropcmaster@gmail.com>
2022-12-26 18:10:28 +0200

    mods

--- resources/views/backend/courses/action.blade.php
+++ resources/views/backend/courses/action.blade.php
@@ -6,7 +6,6 @@
     <i class="la la-edit"></i>
 </a>
 
-<a class="btn btn-sm btn-clean btn-icon btn-icon-md destroy"  id="{{$id}}" data-token="{{ csrf_token() }}" data-route="{{ route('destroycourses', $id) }}">
-
+<a class="btn btn-sm btn-clean btn-icon btn-icon-md destroy"  id="{{$id}}" data-token="{{ csrf_token() }}" href="{{ route('destroycourses', $id) }}">
     <i class="la la-trash-o"></i>
 </a>
