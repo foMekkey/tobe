d4652c360b526d4352870742e2aae8d03eb4c567
fouad mekkey <codepropcmaster@gmail.com>
2022-12-26 18:54:26 +0200

    mods

--- resources/views/backend/courses/action.blade.php
+++ resources/views/backend/courses/action.blade.php
@@ -6,7 +6,7 @@
     <i class="la la-edit"></i>
 </a>
 
-<a class="btn btn-sm btn-clean btn-icon btn-icon-md destroy"  id="{{$id}}" data-token="{{ csrf_token() }}" data-route="{{ route('destroycourses', $id) }}">
+<a class="btn btn-sm btn-clean btn-icon btn-icon-md destroy"  id="{{$id}}" data-token="{{ csrf_token() }}" data-route="{{ URL::to('courses/delete/'.$id) }}">
 
     <i class="la la-trash-o"></i>
 </a>
