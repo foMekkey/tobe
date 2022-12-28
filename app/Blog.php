6105509cc7a6c510cfcd29c65c91ca3086d75aa7
fouad mekkey <codepropcmaster@gmail.com>
2022-12-28 02:15:09 +0200

    mods

--- app/Blog.php
+++ app/Blog.php
@@ -8,4 +8,9 @@ class Blog extends Model
 {
     protected $table = 'blog';
     protected $guarded = [];
+
+    public function getImageAttribute()
+    {
+        return config('filessystems/disks.contabo.url') . $this->image;
+    }
 }
