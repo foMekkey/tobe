83d23561029f45ea9da1878908c6f9d83151694c
fouad mekkey <codepropcmaster@gmail.com>
2023-01-08 17:39:21 +0200

    mods

--- app/Http/Controllers/Admin/CoursesLessonsController.php
+++ app/Http/Controllers/Admin/CoursesLessonsController.php
@@ -94,7 +94,7 @@ class CoursesLessonsController extends Controller
         $courseGroupsUsers = GroupMember::whereIn('group_id', $courseGroups)->distinct('student_id')->pluck('student_id')->toArray();
         $users = CoursesUser::where('course_id', $id)->pluck('user_id')->toArray();
         $totalUsers = array_unique(array_merge($courseGroupsUsers, $users));
-        $usersData = User::whereIn('id', $totalUsers)->get();
+        $usersData = User::whereIn('id', $totalUsers)->with('CoursesUsers')->get();
         return \DataTables::of($usersData)
 
             ->editColumn('type', function ($query) {
