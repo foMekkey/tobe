5c6c0a55d651282971c21b4775d0eee83b6fb9eb
fouad mekkey <codepropcmaster@gmail.com>
2023-01-08 17:25:18 +0200

    mods

--- app/Http/Controllers/Admin/CoursesLessonsController.php
+++ app/Http/Controllers/Admin/CoursesLessonsController.php
@@ -93,8 +93,6 @@ class CoursesLessonsController extends Controller
         $courseGroups = $courses->groups()->pluck('group_id')->toArray();
         $courseGroupsUsers = GroupMember::whereIn('group_id', $courseGroups)->pluck('student_id')->toArray();
         $users = CoursesUser::where('course_id', $id)->whereIn('user_id', $courseGroupsUsers)->distinct('user_id')->get();
-        print_r($users);
-        return;
         return \DataTables::of($users)
 
             ->editColumn('type', function ($query) {
