53a324383301f54cc1407e8b1aef3f3af80cb357
fouad mekkey <codepropcmaster@gmail.com>
2023-01-31 01:47:05 +0200

    mods

--- app/Http/Controllers/Admin/FilesController.php
+++ app/Http/Controllers/Admin/FilesController.php
@@ -46,7 +46,7 @@ class FilesController extends Controller
         $file = File::find($id);
 
         \Storage::disk('contabo')->delete(config("filesystems.disks.contabo.url") . '/' . $file->url);
-
+        return config("filesystems.disks.contabo.url") . '/' . $file->url;
         $check = $file->delete();
 
         if ($check) {
