93c95bd138418397bcd35088142a6321342c43bb
fouad mekkey <codepropcmaster@gmail.com>
2023-03-01 17:03:05 +0200

    mail config

--- resources/views/site/auth/passwords/email.blade.php
+++ resources/views/site/auth/passwords/email.blade.php
@@ -16,9 +16,9 @@
     <section class="login">
         <div class="container-fluid">
             <div class="login_content">
-                <h3>{{ __('site.forgot_password_message') }}</h3>
+                <h3> إسترجاع كلمة المرور</h3>
                 <form action="{{ route('password.email') }}" method="post">
-                    {{csrf_field()}}
+                    {{ csrf_field() }}
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -26,7 +26,9 @@
                     @endif
                     <div class="form-group">
                         <label>{{ __('site.email') }}</label>
-                        <input type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus name="email" placeholder="{{ __('site.email') }}">
+                        <input type="text" class="form-control @error('email') is-invalid @enderror" name="email"
+                            value="{{ old('email') }}" required autocomplete="email" autofocus name="email"
+                            placeholder="{{ __('site.email') }}">
                         <span class="fa fa-envelope"></span>
                         @error('email')
                             <div class="invalid-feedback text-center text-danger" role="alert">
@@ -36,8 +38,9 @@
                     </div>
                     <div class="form-group">
                         <input type="hidden" name="referrer_url" value="{{ Session::get('referrer_url') ?? '' }}" />
-                        <input type="submit" class="btn black_hover" value="{{ __('site.forgot_password_message') }}">
-                        <h5 class="register">{{ __('site.do_not_have_account') }} <a href="{{route('register')}}">{{ __('site.register_account') }}</a></h5>
+                        <input type="submit" class="btn black_hover" value="ارسال">
+                        <h5 class="register">{{ __('site.do_not_have_account') }} <a
+                                href="{{ route('register') }}">{{ __('site.register_account') }}</a></h5>
                     </div>
                 </form>
             </div>
