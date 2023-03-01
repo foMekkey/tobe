94cb157d5c6eb1bc90f8c9b08f7da5611aba236c
fouad mekkey <codepropcmaster@gmail.com>
2023-03-01 18:35:14 +0200

    mods

---
+++ resources/views/emails/active.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+
+<h2>{!! $title !!}</h2>
+
+<br>
+<div>
+</div>
+
+</body>
+</html>
---
+++ resources/views/emails/subscription_accepted.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+
+<h2>{!! $title !!}</h2>
+<p> {!! $data->f_name !!}</p>
+<br>
+<div>
+</div>
+
+</body>
+</html>
