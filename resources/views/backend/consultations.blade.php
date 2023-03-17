88649ba46a41bd5abe0beb8a1865572ea8e99944
fouad mekkey <codepropcmaster@gmail.com>
2023-03-17 04:41:33 +0200

    mods

--- resources/views/backend/consultations.blade.php
+++ resources/views/backend/consultations.blade.php
@@ -1,13 +1,15 @@
 @extends('backend.layouts.app')
 @section('page-main-title', __('pages.consultations'))
-@section('page-main-url', route('consultations') )
+@section('page-main-url', route('consultations'))
 
 
 @section('style')
-    <link href="{{ asset('admin/assets/vendors/custom/datatables/datatables.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('admin/assets/vendors/custom/datatables/datatables.bundle.css') }}" rel="stylesheet"
+        type="text/css" />
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.0.3/css/buttons.dataTables.min.css">
     <style>
-        .nav-pills, .nav-tabs {
+        .nav-pills,
+        .nav-tabs {
             margin: 0;
         }
     </style>
@@ -24,7 +26,7 @@
             <div class="kt-portlet__head-label">
                 <ul id="myTab" class="nav nav-pills nav-pills-sm nav-pills-label nav-pills-bold" role="tablist">
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('services') }}">
+                        <a class="nav-link">
                             <h5>{{ __('pages.services') }}</h5>
                         </a>
                     </li>
@@ -58,9 +60,24 @@
                             <h5>{{ __('pages.store-newsletter') }}</h5>
                         </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link active">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ route('consultations') }}">
                             <h5>{{ __('pages.consultations') }}</h5>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('faqs') }}">
+                            <h5>{{ __('pages.faqs') }}</h5>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('e_wallets') }}">
+                            <h5>{{ __('pages.e_wallets') }}</h5>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('banks') }}">
+                            <h5>{{ __('pages.banks') }}</h5>
                         </a>
                     </li>
                 </ul>
@@ -68,14 +85,21 @@
         </div>
         <div class="kt-portlet__body">
             <!--begin: Datatable -->
-             {!! $dataTable->table(['class' => 'table table-striped- table-bordered table-hover table-checkable', 'id' => 'consult_datatable', 'width' => '100%'],true) !!}
+            {!! $dataTable->table(
+                [
+                    'class' => 'table table-striped- table-bordered table-hover table-checkable',
+                    'id' => 'consult_datatable',
+                    'width' => '100%',
+                ],
+                true,
+            ) !!}
             <!--end: Datatable -->
         </div>
     </div>
-    
+
     <!--begin::Modal-->
     <div class="modal fade" id="kt_modal_consult" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-         aria-hidden="true">
+        aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
@@ -85,7 +109,8 @@
                 </div>
                 <form id="consult_form">
                     <div class="modal-body">
-                        <div class="alert alert-danger" id="consult_error" style="display: none">الرجاء ملىء جميع الحقول</div>
+                        <div class="alert alert-danger" id="consult_error" style="display: none">الرجاء ملىء جميع الحقول
+                        </div>
                         <div class="form-group">
                             <label for="status" class="form-control-label">{{ __('pages.status-column') }}:</label>
                             <select class="form-control" name="status" id="status">
@@ -102,7 +127,8 @@
                     <div class="modal-footer">
                         <input type="hidden" id="consult_id" name="consult_id" value="" />
                         <input type="submit" class="btn btn-primary" value="{{ __('pages.save') }}" />
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">{{ __('pages.close') }}</button>
+                        <button type="button" class="btn btn-secondary"
+                            data-dismiss="modal">{{ __('pages.close') }}</button>
                     </div>
                 </form>
             </div>
@@ -117,20 +143,27 @@
     <script src="https://cdn.datatables.net/buttons/1.0.3/js/dataTables.buttons.min.js"></script>
     <script src="{{ asset('/vendor/datatables/buttons.server-side.js') }}"></script>
     <!--begin::Page Vendors(used by this page) -->
-    <script src="{{ asset('admin/assets/vendors/custom/datatables/datatables.bundle.js') }}" type="text/javascript"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/responsive/2.2.1/js/dataTables.responsive.min.js"></script>
+    <script src="{{ asset('admin/assets/vendors/custom/datatables/datatables.bundle.js') }}" type="text/javascript">
+    </script>
+    <script type="text/javascript" src="https://cdn.datatables.net/responsive/2.2.1/js/dataTables.responsive.min.js">
+    </script>
     <!--end::Page Vendors -->
     <!--begin::Page Scripts(used by this page) -->
-    <script src="{{ asset('admin/assets/js/demo1/pages/crud/datatables/advanced/column-rendering.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('admin/assets/js/demo1/pages/crud/datatables/advanced/column-rendering.js') }}"
+        type="text/javascript"></script>
     {!! $dataTable->scripts() !!}
-    
+
     <script>
         $.fn.datepicker.dates['ar'] = {
             days: ["الأحد", "الاثنين", "الثلاثاء", "الأربعاء", "الخميس", "الجمعة", "السبت", "الأحد"],
             daysShort: ["أحد", "اثنين", "ثلاثاء", "أربعاء", "خميس", "جمعة", "سبت", "أحد"],
             daysMin: ["ح", "ن", "ث", "ع", "خ", "ج", "س", "ح"],
-            months: ["يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"],
-            monthsShort: ["يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"],
+            months: ["يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر",
+                "نوفمبر", "ديسمبر"
+            ],
+            monthsShort: ["يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر",
+                "نوفمبر", "ديسمبر"
+            ],
             today: "هذا اليوم",
             rtl: true
         };
@@ -138,7 +171,7 @@
             language: 'ar',
             format: "yyyy-mm-dd",
         });
-        
+
         $('#status').change(function() {
             if ($(this).val() == 2) {
                 $('#suggested_date').show();
@@ -147,15 +180,15 @@
             }
             $('#consult_error').hide();
         });
-        
+
         $(document).on('click', '.reply_btn', function() {
             $('#consult_id').val($(this).data('rel'));
             $('#status').val('');
             $('#suggested_date').hide();
         });
-        
-        
-        $("#consult_form").submit(function (event) {
+
+
+        $("#consult_form").submit(function(event) {
             // Stop form from submitting normally
             event.preventDefault();
 
@@ -167,9 +200,14 @@
             $.ajax({
                 type: "POST",
                 url: "{{ url('consultations/reply') }}",
-                data: {'_token': "{{ csrf_token() }}", 'id': $('#consult_id').val(), 'status': $('#status').val(), 'suggested_date': $('#datepickerr').val()},
+                data: {
+                    '_token': "{{ csrf_token() }}",
+                    'id': $('#consult_id').val(),
+                    'status': $('#status').val(),
+                    'suggested_date': $('#datepickerr').val()
+                },
                 dataType: "json",
-                success: function (msg) {
+                success: function(msg) {
                     if (msg.success) {
                         $('#kt_modal_consult').modal('hide');
                         location.reload();
