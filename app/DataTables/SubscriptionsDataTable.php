44cc265e0a1b5a2c92f7c4bb8ee8a3d931fed5d8
fouad mekkey <codepropcmaster@gmail.com>
2023-03-18 02:21:42 +0200

    mods

--- app/DataTables/SubscriptionsDataTable.php
+++ app/DataTables/SubscriptionsDataTable.php
@@ -21,40 +21,37 @@ class SubscriptionsDataTable extends DataTable
         return datatables($query)
 
             ->editColumn('action', 'backend.subscriptions.action')
-            
-            ->editColumn('payment_method',function($query){
-               if($query->payment_method == 0)
-                {
-                    return '<span class="kt-badge  kt-badge--success kt-badge--inline kt-badge--pill">'.__('pages.bank_transfer').'</span>';
-                }else{
-                     return '<span class="kt-badge kt-badge--brand kt-badge--inline kt-badge--pill">'.__('pages.e_wallet_transfer').'</span>';
-                }
-            })
-            ->editColumn('status',function($query){
-               if($query->status == 0)
-                {
-                    return '<span class="kt-badge kt-badge--brand kt-badge--inline kt-badge--pill">'.__('pages.pending').'</span>';
-                }else if($query->status == 1){
-                    return '<span class="kt-badge  kt-badge--success kt-badge--inline kt-badge--pill">'.__('pages.accepted').'</span>';
 
-                }else{
-                    return '<span class="kt-badge  kt-badge--success kt-badge--inline kt-badge--pill">'.__('pages.rejected').'</span>';
+            ->editColumn('payment_method', function ($query) {
+                if ($query->payment_method == 0) {
+                    return '<span class="kt-badge  kt-badge--success kt-badge--inline kt-badge--pill">' . __('pages.bank_transfer') . '</span>';
+                } else {
+                    return '<span class="kt-badge kt-badge--brand kt-badge--inline kt-badge--pill">' . __('pages.e_wallet_transfer') . '</span>';
                 }
-                
             })
-            ->editColumn('course_id',function($query){
-               $course = Courses::find($query->course_id);
-               return $course->name;
+            ->editColumn('status', function ($query) {
+                if ($query->status == 0) {
+                    return '<span class="kt-badge kt-badge--brand kt-badge--inline kt-badge--pill">' . __('pages.pending') . '</span>';
+                } else if ($query->status == 1) {
+                    return '<span class="kt-badge  kt-badge--success kt-badge--inline kt-badge--pill">' . __('pages.accepted') . '</span>';
+                } else {
+                    return '<span class="kt-badge  kt-badge--success kt-badge--inline kt-badge--pill">' . __('pages.rejected') . '</span>';
+                }
             })
-            ->editColumn('user_id',function($query){
+            ->editColumn('course_id', function ($query) {
+                $course = Courses::find($query->course_id);
+                return $course->name;
+            })
+            ->editColumn('user_id', function ($query) {
                 $user = \App\User::find($query->user_id);
                 if ($user) {
-                   return $user->edit_link;
+                    return $user->edit_link;
                 }
-                
+
                 return '';
             })
-            ->rawColumns(['action','user_id', 'course_id','payment_method', 'status'])
+            ->rawColumns(['action', 'user_id', 'course_id', 'payment_method', 'status'])
+            ->setRowId('id')
             ->addIndexColumn();
     }
 
@@ -66,8 +63,7 @@ class SubscriptionsDataTable extends DataTable
      */
     public function query(Subscription $model)
     {
-            return $model->newQuery()->select('id','user_id', 'course_id','payment_method', 'status', 'created_at');
-
+        return $model->newQuery()->select('id', 'user_id', 'course_id', 'payment_method', 'status', 'created_at');
     }
 
     /**
@@ -86,8 +82,8 @@ class SubscriptionsDataTable extends DataTable
                 'order'   => [[0, 'desc']],
                 "lengthMenu" => [[10, 25, 50, -1], [10, 25, 50, "All"]],
                 'buttons' => [
-                    ['extend' => 'excel', 'text' => '<i class="fa fa-download"></i>Excel' , 'className' =>'dt-button buttons-copy buttons-html5 btn btn-default legitRipple'],
-                    ['extend' => 'print' , 'text' => '<i class="fa fa-print"></i>Print' , 'className' =>'dt-button buttons-copy buttons-html5 btn btn-default legitRipple'],
+                    ['extend' => 'excel', 'text' => '<i class="fa fa-download"></i>Excel', 'className' => 'dt-button buttons-copy buttons-html5 btn btn-default legitRipple'],
+                    ['extend' => 'print', 'text' => '<i class="fa fa-print"></i>Print', 'className' => 'dt-button buttons-copy buttons-html5 btn btn-default legitRipple'],
 
                 ],
                 'language' => ['url' => asset('ar-datatable.json')],
@@ -102,16 +98,15 @@ class SubscriptionsDataTable extends DataTable
     protected function getColumns()
     {
         $cols =  [
-            'DT_RowIndex' => ['name' => 'id' ,'data' => 'DT_RowIndex' ,'title' => '#'],
-            'user_id' => ['name' => 'user_id' ,'data' => 'user_id' , 'title' => __('pages.user')],
-            'course_id' => ['name' => 'course_id' ,'data' => 'course_id' , 'title' => __('pages.course')],
-            'payment_method' => ['name' => 'payment_method' ,'data' => 'payment_method' , 'title' => __('pages.payment_method')],
-            'status' => ['name' => 'status' ,'data' => 'status' , 'title' => __('pages.status')],
-            'created_at' => ['name' => 'created_at' ,'data' => 'created_at' , 'title' => __('pages.register_at')],
-            'action' => [ 'exportable' => false, 'printable'  => false, 'searchable' => false, 'orderable'  => false, 'title' => __('pages.action')]
+            'DT_RowIndex' => ['name' => 'id', 'data' => 'DT_RowIndex', 'title' => '#'],
+            'user_id' => ['name' => 'user_id', 'data' => 'user_id', 'title' => __('pages.user')],
+            'course_id' => ['name' => 'course_id', 'data' => 'course_id', 'title' => __('pages.course')],
+            'payment_method' => ['name' => 'payment_method', 'data' => 'payment_method', 'title' => __('pages.payment_method')],
+            'status' => ['name' => 'status', 'data' => 'status', 'title' => __('pages.status')],
+            'created_at' => ['name' => 'created_at', 'data' => 'created_at', 'title' => __('pages.register_at')],
+            'action' => ['exportable' => false, 'printable'  => false, 'searchable' => false, 'orderable'  => false, 'title' => __('pages.action')]
         ];
         return $cols;
-
     }
 
     /**
