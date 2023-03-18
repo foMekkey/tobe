31ac0cfc69ce64b924b8a4d504e7c34d0c20af9a
fouad mekkey <codepropcmaster@gmail.com>
2023-03-18 04:57:38 +0200

    mods

--- resources/views/site/contact.blade.php
+++ resources/views/site/contact.blade.php
@@ -1,11 +1,11 @@
 @extends('site.layouts.app')
 
 @section('styles')
-<style>
-    .alert {
-        font-size: 20px;
-    }
-</style>
+    <style>
+        .alert {
+            font-size: 20px;
+        }
+    </style>
 @endsection
 
 @section('content')
@@ -21,7 +21,7 @@
             </ul>
         </div>
     </section>
-    
+
     <section class="contact">
         <div class="container-fluid">
             <div class="row">
@@ -30,14 +30,16 @@
                     <div id="add_contact-alert" class="alert alert-success" role="alert" style="display: none"></div>
                     <div class="col-md-6 col-sm-6 col-xs-12">
                         <div class="form-group">
-                            <input type="text" class="form-control" placeholder="{{ __('site.your_name') }}" name="name">
+                            <input type="text" class="form-control" placeholder="{{ __('site.your_name') }}"
+                                name="name">
                             <span class="fa fa-user"></span>
                         </div>
                     </div>
 
                     <div class="col-md-6 col-sm-6 col-xs-12">
                         <div class="form-group">
-                            <input type="email" class="form-control" placeholder="{{ __('site.your_email') }}" name="email">
+                            <input type="email" class="form-control" placeholder="{{ __('site.your_email') }}"
+                                name="email">
                             <span class="fa fa-envelope"></span>
                         </div>
                     </div>
@@ -48,6 +50,15 @@
                             <span class="fa fa-pen"></span>
                         </div>
                     </div>
+
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="form-group">
+                            <div class="g-recaptcha" id="feedback-recaptcha"
+                                data-sitekey="{{ env('GOOGLE_RECAPTCHA_KEY') }}">
+                            </div>
+                        </div>
+                    </div>
+
                     <div class="col-md-12 col-sm-12 col-xs-12">
                         <p>
                             * {{ __('site.contact_agreement') }}
@@ -68,23 +79,28 @@
 
 @section('scripts')
     <script>
-        $('#add_contact_form').validate({rules: {
-            name: 'required',
-            email: {required: true, email: true},
-            message: 'required'
-        }});
-    
-        $("#add_contact_form").submit(function (event) {
+        $('#add_contact_form').validate({
+            rules: {
+                name: 'required',
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: 'required'
+            }
+        });
+
+        $("#add_contact_form").submit(function(event) {
             // Stop form from submitting normally
             event.preventDefault();
 
-            if(!$(this).valid()) return false;
-            
+            if (!$(this).valid()) return false;
+
             $('#add_contact').prop("disabled", true);
             $('#add_contact').html(
                 `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...`
             );
-    
+
             var formData = new FormData($(this)[0]);
 
             $.ajax({
@@ -94,7 +110,7 @@
                 dataType: 'json',
                 processData: false,
                 contentType: false,
-                success: function (data) {
+                success: function(data) {
                     if (data.success) {
                         $('#add_contact-alert').removeClass('alert-danger');
                         $('#add_contact-alert').addClass('alert-success');
@@ -107,10 +123,10 @@
                         $('#add_contact-alert').text("{{ __('site.add_contact_error') }}");
                         $('#add_contact-alert').show();
                     }
-                    
+
                     $('#add_contact').prop("disabled", false);
                     $('#add_contact').text("{{ __('site.send') }}");
-                    
+
                     $('html, body').animate({
                         scrollTop: $("#add_contact-alert").offset().top - 50
                     }, 1000);
