19a2cbec546524579347574bb455fc5b28ba758e
fouad mekkey <codepropcmaster@gmail.com>
2023-03-19 05:07:18 +0200

    mods

--- app/Http/Controllers/Site/UserController.php
+++ app/Http/Controllers/Site/UserController.php
@@ -27,7 +27,7 @@ class UserController extends Controller
             \DB::table('sessions')->where('user_id', $getUserByEmail->id)->delete();
         if (auth()->attempt($request->only(['email', 'password']), $request->rememberme)) {
             if ($getUserByEmail->email_verified_at === null) {
-                throw ValidationException::withMessages(['email' => 'لم يتم التحقق من البريد الإلكتروني فضلاً قم بتأكيد البريد الإلكتروني الخاص بك من خلال الضغط علي زر التحقق من خلال البريد الإلكتروني الخاص بك']);
+                return ValidationException::withMessages(['email' => 'لم يتم التحقق من البريد الإلكتروني فضلاً قم بتأكيد البريد الإلكتروني الخاص بك من خلال الضغط علي زر التحقق من خلال البريد الإلكتروني الخاص بك']);
             }
             if (!empty($request->referrer_url)) {
                 return redirect($request->referrer_url);
