18edada3747563d022a5b81cbe29515e8ba27c65
fouad mekkey <codepropcmaster@gmail.com>
2023-03-22 01:43:00 +0200

    mods

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -178,7 +178,7 @@ class HomeController extends Controller
             })
 
             ->addColumn('options', function ($query) {
-                $id = $query->email;
+                $id = urlencode($query->email);
                 return view('backend.newsletters.action', compact('id'));
             })
             ->rawColumns(['options', 'type'])
