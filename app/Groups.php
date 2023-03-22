6d159c306e25453bd8c6725cb42a0c74bdd31813
fouad mekkey <codepropcmaster@gmail.com>
2023-03-22 05:35:13 +0200

    mods

--- app/Groups.php
+++ app/Groups.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Groups extends Model
 {
     protected $table = 'groups';
-    protected $fillable = ['name', 'desc', 'key', 'trainer_id', 'tags'];
+    protected $fillable = ['name', 'desc', 'key', 'status', 'trainer_id', 'tags'];
 }
